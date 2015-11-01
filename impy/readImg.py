40b427d0f94e7d65354428f90a4f49f2de5b6633
buptweixin <weixin@bupt.edu.cn>
2015-11-01 13:34:42 +0800

    renew at 11.01

--- impy/readImg.py
+++ impy/readImg.py
@@ -2,6 +2,7 @@
 
 import struct
 from math import ceil
+import StringIO
 
 FORMAT_OFFSET = int('0',16)
 SIZE_OFFSET = int('2',16)
@@ -52,9 +53,59 @@ class readImg:
         bitmap = bitmap[::-1]
         return bitmap
 
+    def saveBitmap(self, filename):
+        raw_copy = StringIO.StringIO()
+        bitmap = self.bitmap[::-1]
+
+        width_bytes = self.width*(self.bpp/8)
+        rowstride = ceil(width_bytes/4.0)*4
+        padding = int(rowstride - width_bytes)
+
+        # Same header as before until the width
+        raw_copy.write(self.raw_data[:WiDTH_OFFSET])
+
+        s = struct.Struct("<i")
+        _w = s.pack(self.width)   # Transform width, height to
+        _h = s.pack(self.height)  # little indian format
+        raw_copy.write(_w)
+        raw_copy.write(_h)
+
+        # After the new width and height the header it's the same
+        raw_copy.write(self.raw_data[HEIGHT_OFFSET+4:DATA_OFFSET])
+
+        for y in xrange(self.height):
+            for x in xrange(self.width):
+                r, g, b = bitmap[y][x]
+
+                # Out of range control
+                if r > 255: r = 255
+                if g > 255: g = 255
+                if b > 255: b = 255
+                if r < 0: r = 0
+                if g < 0: g = 0
+                if b < 0: b = 0
+
+                #Char transformation
+                r = chr(r)
+                g = chr(g)
+                b = chr(b)
+
+                raw_copy.write(b + g + r)
+
+            raw_copy.write(chr(0)*padding)
+
+        self.raw_data = raw_copy.getvalue()
+
+        f = open(filename, "w")
+        f.write(self.raw_data)
+        f.close()
+
+
+
 if __name__ == '__main__':
     img = readImg('../bear.bmp')
     bitmap = img.getBitmap()
+    img.saveBitmap("..bear2.bmp")
 
 
 
